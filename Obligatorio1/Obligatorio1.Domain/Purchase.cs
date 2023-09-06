a8bd70e6f0f46c1783fc542e218a384ab97d66d3
chiarasosa <chiarasosa00@gmail.com>
2023-09-06 15:19:58 -0300

    creacion clase  Purchase (Domain)
    Se crean los atributos

---
+++ Obligatorio1/Obligatorio1.Domain/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1.Domain
+{
+    internal class Purchase
+    {
+        public int PurchaseID { get; set; }
+        public User User { get; set; }
+
+        public List<Product> PurchasedProducts { get; set; }
+        
+        public Promo PromoIDApplied { get; set; }
+
+        public DateTime DateOfPurchase { get; set; }
+    }
+}
