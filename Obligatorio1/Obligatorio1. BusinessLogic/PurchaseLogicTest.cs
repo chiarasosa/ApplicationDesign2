234d40893dc48af6aeaf8c9aeec31f362cd7a2ad
chiarasosa <chiarasosa00@gmail.com>
2023-09-06 16:15:17 -0300

    etapa red del test ValidateMoreThan1Item
    falta agregar la clase Prodcut para continuar con el test

---
+++ Obligatorio1/Obligatorio1. BusinessLogic/PurchaseLogicTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obligatorio1.BusinessLogic;
+
+namespace Obligatorio1.BusinessLogic.Test
+{
+    [TestClass]
+    public class PurchaseLogicTest
+    {
+        [TestMethod]
+        public void ValidateMoreThan1Item()
+        {
+            //Arrange
+            PurchaseLogic purchaseLogic = new PurchaseLogic();
+            List<Product> cart = new List<Product>
+            {
+                new Product("Product1", 10.0, "Description", "Brand", "Category", "Color"),
+                new Product("Product2", 10.0, "Description", "Brand", "Category", "Color")
+            };
+
+            //Act
+            purchaseLogic.ValidateMoreThan1Item(cart);
+        }
+    }
+}
---
+++ Obligatorio1/Obligatorio1.BusinessLogic/PurchaseLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1.BusinessLogic
+{
+    public class PurchaseLogic
+    {
+        public PurchaseLogic()
+        {
+
+        }
+    }
+}
