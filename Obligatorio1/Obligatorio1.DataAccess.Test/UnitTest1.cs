88fa3c8e9be0cfcedbd46b6b97992a1d5380ce7d
chiarasosa <chiarasosa00@gmail.com>
2023-09-06 14:27:11 -0300

    creacion proyectos test

---
+++ Obligatorio1/Obligatorio1.DataAccess.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Obligatorio1.DataAccess.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Obligatorio1/Obligatorio1.WebApi.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Obligatorio1.WebApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
