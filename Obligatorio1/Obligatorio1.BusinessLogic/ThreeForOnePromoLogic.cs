4841258003bc90fc32b48506e51d2690fa062666
chiarasosa <chiarasosa00@gmail.com>
2023-09-08 18:21:44 -0300

    creacion clases e interfaces de Promos

---
+++ Obligatorio1/Obligatorio1.BusinessLogic/ThreeForOnePromoLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Obligatorio1.Domain;
+using Obligatorio1.IBusinessLogic;
+
+namespace Obligatorio1.BusinessLogic
+{
+    public class ThreeForOnePromoLogic : IPromoLogic
+    {
+        public double CalculateDiscount(List<Product> cart)
+        {
+            return 0;
+        }
+    }
+}
