1cacd429eff0e07335fbeebd398fca7a6abcf1c7
fiioro25 <fiioro25@gmail.com>
2023-09-09 10:10:37 -0300

    Se crea estructura para la entidad User
    Se crean las clases e interfaces necesarias dentro de los proyectos de la solucion para manejar la entidad User y sus operaciones.

---
+++ Obligatorio1/Obligatorio1.BusinessLogic.Test/UserTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1.BusinessLogic.Test
+{
+    [TestClass]
+    internal class UserTest
+    {
+    }
+}
---
+++ Obligatorio1/Obligatorio1.BusinessLogic/UserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Obligatorio1.Domain;
+using Obligatorio1.IBusinessLogic;
+
+namespace Obligatorio1.BusinessLogic
+{
+    internal class UserService: IUserService
+    {
+        public UserService() { }
+    }
+}
