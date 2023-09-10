8009db45026a1cd3cd83ba96feea7de87dcef5b9
joaquincalvo <joaquincalvo7@gmail.com>
2023-09-10 17:43:41 -0300

    creacion test AddUser

--- Obligatorio1/Obligatorio1.BusinessLogic.Test/ProductServiceTest.cs
+++ Obligatorio1/Obligatorio1.BusinessLogic.Test/ProductServiceTest.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Obligatorio1.BusinessLogic;
-
+using Obligatorio1.Domain;
+using Obligatorio1.IBusinessLogic;
+using Obligatorio1.IDataAccess;
 
 namespace Obligatorio1.BusinessLogic.Test
 {
+    [TestClass]
     public class ProductServiceTest
     {
 
+        [TestMethod]
+        public void AddProduct()
+        {
+            Mock<IProductManagment>? mock=new Mock<IProductManagment>(MockBehavior.Strict);
+            ProductService service= new ProductService(mock.Object);
+            List<string> aux = new List<string>();
+            aux.Add("azul");
+            aux.Add("rojo");
+            Product prod = new Product("jabon", 120, "sin descripcion", 1, 2,aux);
 
 
+            mock!.Setup(m => m.AddProduct(prod!));
+            service!.AddProduct(prod!);
+
+            mock.VerifyAll();
+        }
 
     }
 }
