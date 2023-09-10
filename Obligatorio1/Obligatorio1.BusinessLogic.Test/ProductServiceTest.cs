2523783e44ce388c16a05903750dc51a4d49c0fc
joaquincalvo <joaquincalvo7@gmail.com>
2023-09-10 12:22:08 -0300

    productTest

---
+++ Obligatorio1/Obligatorio1.BusinessLogic.Test/ProductServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Obligatorio1.BusinessLogic;
+
+
+namespace Obligatorio1.BusinessLogic.Test
+{
+    public class ProductServiceTest
+    {
+
+
+
+
+    }
+}
