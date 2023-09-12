b8acda3dff26b0c13288dd566bc51707849f8771
chiarasosa <chiarasosa00@gmail.com>
2023-09-12 14:25:44 -0300

    purchase publica

--- Obligatorio1/Obligatorio1.Domain/Purchase.cs
+++ Obligatorio1/Obligatorio1.Domain/Purchase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Obligatorio1.Domain
 {
-    internal class Purchase
+    public class Purchase
     {
         public int PurchaseID { get; set; }
         //public User User { get; set; }
