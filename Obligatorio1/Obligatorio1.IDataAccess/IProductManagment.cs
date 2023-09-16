48bd8597b312f313c968d6363b38ba7fc97a349c
joaquincalvo <joaquincalvo7@gmail.com>
2023-09-16 16:46:16 -0300

    cambios

--- Obligatorio1/Obligatorio1.IDataAccess/IProductManagment.cs
+++ Obligatorio1/Obligatorio1.IDataAccess/IProductManagment.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Obligatorio1.IDataAccess;
 using Obligatorio1.Domain;
 
 namespace Obligatorio1.IDataAccess
 {
     public interface IProductManagment
     {
+
         void RegisterProduct(Product product);
         Product UpdateProduct(Product product);
         Product GetProductByID(int productID);
         IEnumerable<Product> GetProducts();
+
     }
 }
