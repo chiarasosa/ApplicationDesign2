cc1ab439a2f5e4935516e5951cde2c2507fc2624
fiioro25 <fiioro25@gmail.com>
2023-09-17 16:59:10 -0300

    WebApi

---
+++ Obligatorio1/Obligatorio1.WebApi/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Obligatorio1.IBusinessLogic;
+using Obligatorio1.Domain;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult RegisterUser([FromBody] User user)
+    {
+        try
+        {
+            _userService.RegisterUser(user);
+            return Ok("Usuario registrado exitosamente.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Error al registrar el usuario: {ex.Message}");
+        }
+    }
+}
---
+++ Obligatorio1/Obligatorio1.WebApi/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting; // Importa este espacio de nombres
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Obligatorio1.IBusinessLogic;
+using Obligatorio1.BusinessLogic;
+
+namespace Obligatorio1.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Este método se utiliza para configurar los servicios que tu aplicación utilizará.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configuraciones de servicios aquí...
+            services.AddScoped<IUserService, UserService>();
+          
+
+        }
+
+        // Este método se utiliza para configurar cómo se manejarán las solicitudes HTTP entrantes.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configuraciones de middleware y enrutamiento aquí...
+        }
+    }
+}
