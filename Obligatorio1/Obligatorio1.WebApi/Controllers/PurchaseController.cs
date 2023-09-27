d1b7ce77e88ec3d56c63c6775cef65998130ee57
chiarasosa <chiarasosa00@gmail.com>
2023-09-26 22:40:31 -0300

    purchase controller y test

---
+++ Obligatorio1/Obligatorio1.WebApi/Controllers/PurchaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Obligatorio1.IBusinessLogic;
+using Obligatorio1.Domain;
+
+namespace Obligatorio1.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/purchase")]
+    public class PurchaseController : ControllerBase
+    {
+        private readonly IPurchaseService _purchaseService;
+
+        public PurchaseController(IPurchaseService purchaseService)
+        {
+            _purchaseService = purchaseService;
+        }
+
+        [HttpGet]
+        public IActionResult ValidateMoreThan1Item(Purchase purchase)
+        {
+            // Call the ValidateMoreThan1Item method from the service
+            bool validPurchase = _purchaseService.ValidateMoreThan1Item(purchase);
+
+            // Return the result as needed, for example, as an OkObjectResult
+            return Ok(validPurchase);
+        }
+    }
+}
