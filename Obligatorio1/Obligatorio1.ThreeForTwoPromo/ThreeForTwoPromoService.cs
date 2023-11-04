32dea5b972b53c846deff4e4ac730d7fc0087ce3
chiarasosa <chiarasosa00@gmail.com>
2023-11-04 11:49:41 -0300

    creawcion proyecto 3x2

--- Obligatorio1/Obligatorio1.ThreeForTwoPromo/ThreeForTwoPromoService.cs
+++ Obligatorio1/Obligatorio1.ThreeForTwoPromo/ThreeForTwoPromoService.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Obligatorio1.Domain;
-using Obligatorio1.IBusinessLogic;
-using Obligatorio1.IDataAccess;
+using Obligatorio1.PromoInterface;
 
 namespace Obligatorio1.BusinessLogic
 {
