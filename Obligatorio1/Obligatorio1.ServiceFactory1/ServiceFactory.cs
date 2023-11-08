52dea30d6459799b059a9892cb1c298c07fe3792
chiarasosa <chiarasosa00@gmail.com>
2023-11-07 23:21:43 -0300

    creacion service factory

---
+++ Obligatorio1/Obligatorio1.ServiceFactory1/ServiceFactory.cs
@@ -0,0 +1,42 @@
+﻿using Obligatorio1.BusinessLogic;
+using Obligatorio1.IBusinessLogic;
+using Obligatorio1.IDataAccess;
+using Obligatorio1.DataAccess;
+using Obligatorio1.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Cors;
+
+namespace Obligatorio1.ServiceFactory1;
+
+    public class ServiceFactory
+    {
+        public ServiceFactory()
+        {
+
+        }
+
+        public void RegistrateServices(IServiceCollection serviceCollection)
+        {/*
+                serviceCollection.AddDbContext<DbContext, Obligatorio1Context>();
+                //serviceCollection.AddScoped<IGenericRepositort<Character>, Character>();
+                //serviceCollection.AddScoped<ICharacterService, CharacterService>();*/
+
+            serviceCollection.AddCors(options =>
+
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                }
+
+                );
+            }
+
+            );
+        }
+    }
