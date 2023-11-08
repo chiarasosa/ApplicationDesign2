fb2b5ddc4ec25c9aea82613d37b48a24b7d97b70
joaquincalvo <joaquincalvo7@gmail.com>
2023-11-08 02:22:17 -0300

    commit

--- ObligatorioAngular/src/app/app.module.ts
+++ ObligatorioAngular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from './componentes/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
