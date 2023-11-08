03f653d74315e63fdfbf210d2bc8296ba3e22e41
joaquincalvo <joaquincalvo7@gmail.com>
2023-11-08 14:28:17 -0300

    img

--- Obligatorio1Angular/src/app/app-routing.module.ts
+++ Obligatorio1Angular/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrarComponent } from './componentes/registrar/registrar.component';
 import { InicioSesionComponent } from './componentes/inicio-sesion/inicio-sesion.component';
+import { BarraComponent } from './componentes/barra/barra.component';
 
 const routes: Routes = [
   { path: 'inicioSesion', component: InicioSesionComponent },
-  {path: 'Registrar', component:RegistrarComponent}
+  {path: 'Registrar', component:RegistrarComponent},
+  {path: 'barra', component:BarraComponent}
+  
 ];
 
 @NgModule({
--- Obligatorio1Angular/src/app/app.module.ts
+++ Obligatorio1Angular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioSesionComponent } from './componentes/inicio-sesion/inicio-sesion.component';
 import { RegistrarComponent } from './componentes/registrar/registrar.component';
+import { LandingPageComponent } from './componentes/landing-page/landing-page.component';
+import { BarraComponent } from './componentes/barra/barra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioSesionComponent,
-    RegistrarComponent
+    RegistrarComponent,
+    LandingPageComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
