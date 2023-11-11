6d62b83bc320aa0bb91133f72f866e0da5771ca0
joaquincalvo <joaquincalvo7@gmail.com>
2023-11-11 00:50:14 -0300

    lista productos

--- Obligatorio1Angular/src/app/componentes/lista-productos/lista-productos.component.ts
+++ Obligatorio1Angular/src/app/componentes/lista-productos/lista-productos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/modelos/Product';
+import { ProductService } from 'src/app/servicios/product.service';
+
 
 @Component({
   selector: 'app-lista-productos',
   templateUrl: './lista-productos.component.html',
   styleUrls: ['./lista-productos.component.css']
 })
-export class ListaProductosComponent {
+export class ListaProductosComponent implements OnInit {
+  products: Product[] | undefined;
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe((products: Product[]) => {
+      this.products = products;
+    });
+}
 }
--- Obligatorio1Angular/src/app/servicios/product.service.ts
+++ Obligatorio1Angular/src/app/servicios/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../modelos/Product';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'https://localhost:7004/api';
+  constructor(private http: HttpClient) { }
+
+  getProducts():Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
 }
