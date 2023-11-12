df118de9ba80df2c61e9bd3e5d16da3526e40446
joaquincalvo <joaquincalvo7@gmail.com>
2023-11-12 19:17:31 -0300

    cerrarSesion

--- Obligatorio1Angular/src/app/componentes/barra/barra.component.ts
+++ Obligatorio1Angular/src/app/componentes/barra/barra.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LocalStorageService } from 'src/app/servicios/localStorage';
+import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
+import { DialogService } from 'src/app/servicios/dialog.service';
 
 @Component({
   selector: 'app-barra',
@@ -11,8 +13,9 @@ import { LocalStorageService } from 'src/app/servicios/localStorage';
 export class BarraComponent {
   userRole: string = '';
   activeRoute: string = '';
+  
 
-  constructor(private router:Router,private localStorageService: LocalStorageService){
+  constructor(private router:Router,private localStorageService: LocalStorageService,private dialogService: DialogService ){
 
     const userData = localStorage.getItem('user');
     if (userData) {
@@ -25,9 +28,18 @@ export class BarraComponent {
   
   logout() {
    this.localStorageService.removeToken();
+   this.openAlertDialog('Éxito', 'Se cerró la sesión');
   }
   
   setActiveRoute(route: string): void {
     this.activeRoute = route;
   }
+
+  openAlertDialog(title: string, message: string) {
+    this.dialogService.openAlertDialog(title, message); // Usa dialogService
+
+    
+
+  }
+  
 }
