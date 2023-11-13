9285c22cc5b795351b41095072bb2498f8b9f916
fiioro25 <fiioro25@gmail.com>
2023-11-13 13:31:21 -0300

    Cambios

--- Obligatorio1Angular/src/app/componentes/barra/barra.component.ts
+++ Obligatorio1Angular/src/app/componentes/barra/barra.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { LocalStorageService } from 'src/app/servicios/localStorage';
 import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
 import { DialogService } from 'src/app/servicios/dialog.service';
@@ -14,9 +13,11 @@ import { CarritoComponent } from 'src/app/componentes/carrito/carrito.component'
 export class BarraComponent {
   userRole: string = '';
   activeRoute: string = '';
-  
+  isLoggedIn: boolean = false; // Variable para controlar si el usuario ha iniciado sesión
 
-  constructor(private router:Router,private localStorageService: LocalStorageService,private dialogService: DialogService ){
+  constructor(private router: Router, private localStorageService: LocalStorageService, private dialogService: DialogService) {
+    // Verifica si hay un token en el LocalStorage para determinar si el usuario ha iniciado sesión
+    this.isLoggedIn = !!this.localStorageService.getToken();
 
     const userData = localStorage.getItem('user');
     if (userData) {
@@ -26,21 +27,18 @@ export class BarraComponent {
       }
     }
   }
-  
+
   logout() {
-   this.localStorageService.removeToken();
-   this.openAlertDialog('Éxito', 'Se cerró la sesión');
+    this.localStorageService.removeToken();
+    this.isLoggedIn = false; // Cuando cierras sesión, establece isLoggedIn en falso
+    this.openAlertDialog('Éxito', 'Se cerró la sesión');
   }
-  
+
   setActiveRoute(route: string): void {
     this.activeRoute = route;
   }
 
   openAlertDialog(title: string, message: string) {
-    this.dialogService.openAlertDialog(title, message); // Usa dialogService
-
-    
-
+    this.dialogService.openAlertDialog(title, message);
   }
-  
 }
--- Obligatorio1Angular/src/app/componentes/inicio-sesion/inicio-sesion.component.ts
+++ Obligatorio1Angular/src/app/componentes/inicio-sesion/inicio-sesion.component.ts
@@ -1,11 +1,9 @@
-// inicio-sesion.component.ts
-import { MatDialog } from '@angular/material/dialog';
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/servicios/user.service';
 import { Router } from '@angular/router';
-import { User } from 'src/app/modelos/User'; // Asegúrate de importar el modelo User adecuado
-import { DialogService } from 'src/app/servicios/dialog.service'; // Asegúrate de importar el servicio DialogService
+import { User } from 'src/app/modelos/User';
 import { LocalStorageService } from 'src/app/servicios/localStorage';
+import { DialogService } from 'src/app/servicios/dialog.service';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -13,29 +11,38 @@ import { LocalStorageService } from 'src/app/servicios/localStorage';
   styleUrls: ['./inicio-sesion.component.css'],
 })
 export class InicioSesionComponent {
-  user: User = new User(0,'', '', '', '', ''); // Crea una nueva instancia de User
+  user: User = new User(0, '', '', '', '', '');
+  isLoggedIn: boolean = false; // Variable para controlar el estado del inicio de sesión
 
-  constructor(private userService: UserService, private router: Router, private dialogService: DialogService, private localStorageService: LocalStorageService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private dialogService: DialogService,
+    private localStorageService: LocalStorageService
+  ) {}
 
   loginUser() {
     this.userService.loginUser(this.user).subscribe(
       (response) => {
-        // Maneja la respuesta del inicio de sesión aquí
         console.log('Inicio de sesión exitoso:', response);
         this.localStorageService.setToken(response.token);
+        window.location.reload();
+        this.isLoggedIn = true;
         this.openAlertDialog('Éxito', 'Inicio de sesión exitoso.');
 
+        // Establece la variable isLoggedIn en true después de iniciar sesión con éxito
+
         // Redirige a la página deseada
-       // this.router.navigate(['/pagina-deseada']); // Reemplaza 'pagina-deseada' con la ruta a la página que deseas después del inicio de sesión
+        // this.router.navigate(['/pagina-deseada']);
       },
       (error) => {
         console.error('Error en el inicio de sesión:', error);
-        this.openAlertDialog('Error', 'Error al iniciar sesion. Intente nuevamente.');
+        this.openAlertDialog('Error', 'Error al iniciar sesión. Intente nuevamente.');
       }
     );
   }
-  
+
   openAlertDialog(title: string, message: string) {
-    this.dialogService.openAlertDialog(title, message); 
+    this.dialogService.openAlertDialog(title, message);
   }
 }
