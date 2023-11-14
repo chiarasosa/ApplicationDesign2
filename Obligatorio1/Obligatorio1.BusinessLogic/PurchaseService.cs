9693b2bf894397ce18f7dc827af98c28df121980
joaquincalvo <joaquincalvo7@gmail.com>
2023-11-13 23:52:25 -0300

    commit

--- Obligatorio1/Obligatorio1.BusinessLogic/PurchaseService.cs
+++ Obligatorio1/Obligatorio1.BusinessLogic/PurchaseService.cs
@@ -56,12 +56,12 @@ namespace Obligatorio1.BusinessLogic
                             ProductID = product.ProductID,
                         };
                         newPurchase.PurchasedProducts.Add(purchaseProduct);
-                        _productService.ProductSold(product);
+                      //  _productService.ProductSold(product);
                     }
-                    else
+                   /* else
                     {
                         cart.Products.Remove(product);
-                    }
+                    }*/
                     
                 }
 
